8360a89eed05fd49cf1e7e925c15e2d0d262ae6a
Saranya-0 <saranyasaseendran25@gmail.com>
2024-11-11 12:24:34 +0530

    Media app server

---
+++ index.js
@@ -0,0 +1,26 @@
+// 1 import json server
+const jsonServer =require('json-server')
+
+// 2 create server application using json server
+const mediaAppServer = jsonServer.create()
+
+// 3 create a middleware used by json Server
+const middleware = jsonServer.defaults()
+
+// 4 create router: set up router for db.json file
+const router = jsonServer.router('db.json')
+
+// 5 applying middleware to server
+mediaAppServer.use(middleware)
+
+// 6 applying router to server
+mediaAppServer.use(router)
+
+// 7 Define port
+const PORT =3000
+
+// 8 Starting the server
+mediaAppServer.listen(PORT,()=>{
+    console.log("Media app server started on PORT" + PORT);
+    
+})
